731567f638c152f63e2d59f0f9c2d4841a8eb184
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-06-22 01:05:11 -0500

    created the relationships to my models in my models index.js file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,13 +1,35 @@
 const User = require('./User');
-const Project = require('./Project');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
+User.hasMany(Blog, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+User.hasMany(Comment, {
   foreignKey: 'user_id'
+})
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
-module.exports = { User, Project };
+Blog.hasMany(Comment, {
+   foreignKey: 'blog_id',
+   onDelete: 'CASCADE' 
+});
+
+Comment.belongsTo(Blog, {
+   foreignKey: 'blog_id',
+   onDelete: 'CASCADE' 
+});
+
+Comment.belongsTo(User, {
+   foreignKey: 'user_id',
+   onDelete: 'CASCADE'
+});
+
+
+module.exports = { User, Blog, Comment };
