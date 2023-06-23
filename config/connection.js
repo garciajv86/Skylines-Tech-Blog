d23ebfbc26d9b2b34f65f997f506ce7bb4f30d0d
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-06-23 12:32:44 -0500

    placed env variables in the file for security

--- config/connection.js
+++ config/connection.js
@@ -4,16 +4,18 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+  //* JAWSDB MySQL connection
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  //* Local MySQL connection
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
+      host: process.env.DB_HOST,
+      port: 3306,
+      dialect: process.env.DB_DIALECT,
     }
   );
 }
